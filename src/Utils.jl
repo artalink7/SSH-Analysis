fe3c3f8ae70da0cecf827642e535e6f50ce818d2
Giuliano <giuliano.artale@polito.it>
2025-08-18 16:36:06 +0200

    Erasing useless inputs and modified Utils
    US01

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,4 +1,7 @@
 function SaveWithTimeStamp(data, base_dir::String, prefix::String)
+    # Ensure the directory exists
+    isdir(base_dir) || mkpath(base_dir)
+
     filename = joinpath(base_dir, "$(prefix).txt")
     writedlm(filename, data)
     println("Saved to: $filename")
