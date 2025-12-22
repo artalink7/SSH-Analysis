0d00965441acce02e39aa023fe3b28794aba37ac
Giuliano <giuliano.artale@polito.it>
2025-12-22 13:44:01 +0100

    Update on simulation free system

--- scripts/equilibrium/run_entropy_over_variance_ssh_vs_rm.jl
+++ scripts/equilibrium/run_entropy_over_variance_ssh_vs_rm.jl
@@ -9,10 +9,10 @@ gap = 10 .^ range(-4, -1; length=20)
 N_cells = 5000  
 #Entropy_SSH = zeros(Float64, length(gap))
 #Variance_SSH = zeros(Float64, length(gap))
-Ratio_SSH = zeros(Float64, length(gap))
+#Ratio_SSH = zeros(Float64, length(gap))
 #Entropy_RM = zeros(Float64, length(gap))
 #Variance_RM = zeros(Float64, length(gap))
-#Ratio_RM = zeros(Float64, length(gap))
+Ratio_RM = zeros(Float64, length(gap))
 #p = Progress(length(gap))
 Threads.@threads for i in eachindex(gap)
     r = gap[i]
@@ -22,14 +22,14 @@ Threads.@threads for i in eachindex(gap)
     # Entropy_RM[i] = EntanglementEntropy([0, 1 , 1, r/2], [0, 1 , 1, r/2], "discrete", N_cells, 2*N_cells, "1", 0)
     # Variance_RM[i] = SingleVariance([0, 1 , 1, r/2], [0, 1 , 1, r/2], "discrete", N_cells, 2*N_cells, "1", 0)
     # Ratio_RM[i] = Entropy_RM[i] / Variance_RM[i]
-    S, V = EntropyandVariance_sub([0, 1 - r/2, 1, 0], [0, 1- r/2, 1, 0], "discrete", N_cells, 2*N_cells, 0)
-    Ratio_SSH[i] = S / V
+    S, V = EntropyandVariance_sub([0, 1 , 1, r/2], [0, 1 , 1, r/2], "discrete", N_cells, 2*N_cells, 0)
+    Ratio_RM[i] = S / V
     #next!(p)
 end 
 
 data_dir = joinpath(ProjectRoot(), "data", "equilibrium")
-SaveWithTimeStamp(Ratio_SSH, data_dir, "ratio_entropy_variance_vs_gap_ssh_1e-4_half_filling_5000cells")
-# SaveWithTimeStamp(Ratio_RM, data_dir, "ratio_entropy_variance_vs_gap_rm_1e-4_half_filling")
+# SaveWithTimeStamp(Ratio_SSH, data_dir, "ratio_entropy_variance_vs_gap_ssh_1e-4_half_filling_5000cells")
+SaveWithTimeStamp(Ratio_RM, data_dir, "ratio_entropy_variance_vs_gap_rm_1e-4_half_filling_5000cells")
 # SaveWithTimeStamp(Entropy_SSH, data_dir, "entropy_vs_gap_ssh_1e-4_half_filling")
 # SaveWithTimeStamp(Variance_SSH, data_dir, "variance_vs_gap_ssh_1e-4_half_filling")
 # SaveWithTimeStamp(Entropy_RM, data_dir, "entropy_vs_gap_rm_1e-4_half_filling")
