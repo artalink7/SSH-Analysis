c2c16bf51df5c0aaaead8af44242fd75cb252f96
artalink7 <giuliano.artale@gmail.com>
2026-05-07 13:57:20 +0200

    update on folder structure

---
+++ scripts/quench/quench_paper/post_quench_gapless/RM_free_quench_gapped_to_gapless_no_flux.jl
@@ -0,0 +1,26 @@
+push!(LOAD_PATH, joinpath(@__DIR__, "..","..","..", "src"))
+using SSHAnalysis
+
+# Saving path
+data_dir = joinpath(ProjectRoot(), "data", "quench", "quench_paper")
+
+
+# Define parameters
+
+pre_quench  = (φ=0, w=1.0, v=1.0, V=0.5)
+post_quench = (φ=0, w=1.0, v=0.99999, V=0.0)
+style = "discrete"
+L_cells = 100
+N_cells = 200      
+
+# Generate Data
+open(data_dir * "/run_RM_free_quench_gapped_to_gapless_v99999_L100cells_200sec.csv", "w") do file 
+    write(file , "Time,Entropy,Variance\n")
+    time = 0:0.1:200
+    for t in time
+        entropy, variance = EntropyandVariance_sub(pre_quench, post_quench, style, L_cells, N_cells, t) 
+        write(file, "$t,$entropy,$variance\n")
+        @printf("t= %.1f, S_A= %.4f, ΔN_A = %.4f\n", t, entropy, variance)
+    end
+end
+
--- scripts/quench/quench_paper/post_quench_gapped/TB_quench_interacting_gapped_to_non_interacting_gapped.jl
+++ scripts/quench/quench_paper/post_quench_gapped/TB_quench_interacting_gapped_to_non_interacting_gapped.jl
@@ -1,27 +1,30 @@
-push!(LOAD_PATH, joinpath(@__DIR__, "..","..","..", "src"))
+push!(LOAD_PATH, joinpath(@__DIR__, "..","..","..","..", "src"))
 using SSHAnalysis
 
 # Saving path
-data_dir = joinpath(ProjectRoot(), "data", "quench")
+data_dir = joinpath(ProjectRoot(), "data", "quench", "quench_paper")
 
 
 # Define parameters
 
-N_sites = 200
+N_sites = 200 # Number of cells is 100, so number of sites is 200
 T_max = 10.0
 dt = 0.05
 v_i = 1.0
-w_i = 0.5
-V_i = 1.0
-v_f = 1.0
-w_f = 0.5
+w_i = 1.0
+V_i = 4.0
+v_f = 0.5
+w_f = 1.0
+
+# gap of the post quench hamiltonia = 1.0 
+
 
 # Run the quench simulation
 times, entropies, variances = simulate_quench(N_sites, T_max, dt; v_i=v_i, w_i=w_i, V_i=V_i, v_f=v_f, w_f=w_f)
 
 # Save the data to a CSV file
 df = DataFrame(Time=times, Entropy=entropies, Variance=variances)
-filename = "run_TB_quench_interacting_to_non_interacting_SSH_200sites_10sec.csv"
+filename = "run_TB_quench_interacting_gapped_to_non_interacting_gapped_SSH_200sites_10sec.csv"
 CSV.write(data_dir * "/$filename", df)
 
 println("Quench simulation completed and data saved to $data_dir/$filename")
