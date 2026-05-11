6dd667f331f5cd17b31afdd078c6877d5c7bd3d6
artalink7 <giuliano.artale@gmail.com>
2026-05-11 11:43:10 +0200

    small update

--- scripts/quench/quench_paper/post_quench_gapless/TB_free_quench_gapless_with_flux_to_gapless_no_flux.jl
+++ scripts/quench/quench_paper/post_quench_gapless/TB_free_quench_gapless_with_flux_to_gapless_no_flux.jl
@@ -10,14 +10,14 @@ data_dir = joinpath(ProjectRoot(), "data", "quench", "quench_paper")
 style = "discrete"
 L_cells = 100
 N_cells = 200
-flux_param = π / N_cells
+flux_param = -π / (2*N_cells)
 
 pre_quench  = (φ=flux_param, w=1.0, v=0.99999, V=0.0)
 post_quench = (φ=0, w=1.0, v=0.99999, V=0.0)
 
 
 # Generate Data
-open(data_dir * "/run_TB_free_quench_gapless_with_flux_to_gapless_no_flux_L100cells_200sec_v99999.csv", "w") do file 
+open(data_dir * "/run_TB_free_quench_gapless_with_flux_to_gapless_no_flux_L100cells_200sec_v99999_neg_pi_div_2N.csv", "w") do file 
     write(file , "Time,Entropy,Variance\n")
     time = 0:0.1:200
     for t in time
