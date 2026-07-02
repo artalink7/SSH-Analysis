c4b8f52163a1af0d34d681c85427cca252960377
artalink7 <giuliano.artale@gmail.com>
2026-07-02 17:12:52 +0200

    small fix

--- scripts/quench/quench_paper/post_quench_gapless/quench_from_luttinger_liquid_to_luttinger_liquid.jl
+++ scripts/quench/quench_paper/post_quench_gapless/quench_from_luttinger_liquid_to_luttinger_liquid.jl
@@ -4,6 +4,10 @@ using SSHAnalysis
 # Saving path
 data_dir = joinpath(ProjectRoot(), "data", "quench", "quench_paper")
 
+# log file
+log_dir = joinpath(ProjectRoot(), "logs", "quench", "quench_paper")
+mkpath(log_dir)  # Create the log directory if it doesn't exist
+log_file = joinpath(log_dir, "quench_from_Luttinger_liquid_to_Luttinger_liquid.log")
 
 # Define parameters
 
@@ -16,7 +20,8 @@ pre_quench = SSHParams(v = 1.0, w= 1.0, Δ = 0, V = 0.5)
 post_quench = SSHParams(v = 1.0, w= 1.0, Δ = 0, V = 1.0)
 
 # Run the quench simulation
-times, entropies, variances = simulate_quench(N_sites, T_max, dt, pre_quench, post_quench; bond_dim=bond_dimension)
+times, entropies, variances = simulate_quench(N_sites, T_max, dt, pre_quench, post_quench; 
+                                                bond_dim=bond_dimension, logfile=log_file)
 
 # Save the data to a CSV file
 df = DataFrame(Time=times, Entropy=entropies, Variance=variances)
